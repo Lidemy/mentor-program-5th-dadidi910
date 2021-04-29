c1868c2dbd2e77c86290976f1051be3feadfe4e0
DidiJhong <didijhong@Didi-Jhong-MacBook-Pro.local>
2021-04-30 01:17:31 +0800

    week2_1

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
+// 反轉字串
+// 從字串的最後一個字開始
+
 function reverse(str) {
-  
+  var result = ''
+  for(let i=str.length-1; i >= 0; i--){
+    result += str[i]
+  }  
+  console.log(result)
 }
 
-reverse('hello');
+reverse('hello')
