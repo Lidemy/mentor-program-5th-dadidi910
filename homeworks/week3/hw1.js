26e6c03eb3e9c20b4697f9347757be1d27d41b72
DidiJhong <didijhong@Didi-Jhong-MacBook-Pro.local>
2021-04-30 21:39:56 +0800

    hw1

---
+++ homeworks/week3/hw1.js
@@ -0,0 +1,29 @@
+// 印出星星
+// 第 i 行有 i 個星星
+
+const readline = require('readline')
+
+const lines = []
+const rl = readline.createInterface({
+  input: process.stdin
+})
+
+rl.on('line', (line) => {
+  lines.push(line)
+})
+
+rl.on('close', () => {
+  solve(lines)
+})
+
+function solve(lines) {
+  const n = lines[0]
+  printStar(n)
+  function printStar(n) {
+    let str = ''
+    for (let i = 1; i <= n; i++) {
+      str += '*'
+      console.log(str)
+    }
+  }
+}
